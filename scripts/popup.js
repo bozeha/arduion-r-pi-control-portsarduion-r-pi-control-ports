efda4b02cb46de8ef49a92968bdfa99a00b765a8
BOZE BOZE <BOZE BOZE>
2017-08-18 19:46:14 +0300

    clean

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,5 @@
 function popUpNow(x)
 {
-//
 
 $('#myModal p').text(x['mess']);
 $('#myModal h4').text(x['title']);
@@ -8,7 +7,5 @@ $('#myModal button').eq(1).text(x['b1']);
 $('#myModal button').eq(1).attr('onclick',"runCommand('"+x['comm']+"')");
 $('#myModal button').eq(2).text(x['b2']);
 $('#myModal').modal();
-console.log(x);
-//runCommand(x['comm']);
 
 }
